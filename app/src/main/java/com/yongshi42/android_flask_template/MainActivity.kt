1f8ea1b27c60048458881233fb1895880c112ed3
Michael-Yongshi <39827427+Michael-Yongshi@users.noreply.github.com>
2020-06-10 10:37:46 +0200

    empty activity

---
+++ app/src/main/java/com/yongshi42/android_flask_template/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yongshi42.android_flask_template
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
